b2ce32f325538308f39b4873f25b68b672dd196e
yangwangjinxing <yangwangjinxing@163.com>
2016-12-11 21:48:22 +0800

    it work

--- dict.py
+++ dict.py
@@ -10,12 +10,12 @@ python3 """+__name__+""" -f [file]  导入词典
 if __name__=='__main__':
     d=dbi.DataBase()
     if len(sys.argv)==2:
-        result= d.find({'word':sys.argv[1]}) or \
-                d.find({'explain':{'$regex':r'\r\n'+sys.argv[1]+r'\W\r'}}) or \
-                d.find({'word':{'$regex':'^'+sys.argv[1]}}) or \
-                d.find({'explain':{'$regex':r'\W'+sys.argv[1]+r'\W'}}) or \
-                d.find({'word':{'$regex':sys.argv[1]}}) or \
-                d.find({'explain':{'$regex':sys.argv[1]}}) 
+        result= d.find({'word':{'$regex':r'^'+sys.argv[1]+r'$',$options:'$i'}) or \
+                d.find({'explain':{'$regex':r'\r\n'+sys.argv[1]+r'\W\r',$options:'$i'}}) or \
+                d.find({'word':{'$regex':'^'+sys.argv[1],$options:'$i'}}) or \
+                d.find({'explain':{'$regex':r'\W'+sys.argv[1]+r'\W',$options:'$i'}}) or \
+                d.find({'word':{'$regex':sys.argv[1],$options:'$i'}}) or \
+                d.find({'explain':{'$regex':sys.argv[1],$options:'$i'}}) 
  #找到单词 or #找到派生 or #单词前缀匹配 or #解析中出现 #单词部分匹配 #解析部分匹配
         if result:
             print(result['word']+'\n'+result['explain'])
